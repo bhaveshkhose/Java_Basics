f8ac9832334379e329c92e73ffb4d86591448b08
bhaveshkhose <khosebhavesh@gmail.com>
2024-12-22 23:52:44 +0530

    New Logic building method has been added

--- Array/ArrayOperation.java
+++ Array/ArrayOperation.java
@@ -3,6 +3,46 @@ import java.util.*;
 
 public class ArrayOperation {
 	
+	/*
+	 * This Method will print this pattren
+	 * if n = 5 then
+	 * 00005
+	 * 0004
+	 * 003
+	 * 02
+	 * 1
+	 */
+	public static void print(int n) {
+				for(int i = 0 ; i < n ; i++) {
+			for(int j = 0 ; j < n -i-1; j++) {
+				System.out.print(0);
+			}	
+			System.out.println(n -i);
+		}
+	}
+	
+	public static void insertElement(int[] nums , int n , int idx) {
+		
+		int[] result = new int[nums.length+1];
+		
+		result = nums;
+		
+		if(idx < 0 || idx > nums.length) {
+			System.out.println("Index out of bound");
+			return ;
+		}
+		
+		for(int i = nums.length -2; i >= idx ; i--) {
+			result[i +1] = result[i];
+		}
+		result[idx] = n;
+		
+		for(int i : result) {
+			System.out.print(i +" ");
+		}
+				
+	}
+ 	
 	public static void printArray1(int[] arr) {
 		System.out.print("[");
 		for(int i =0 ; i < arr.length ; i ++) {
@@ -84,6 +124,12 @@ public class ArrayOperation {
 	public static void main(String [] args) {
 		
 		int[] arr = {4,3,2,5,6};
+		int [] result = new int[arr.length+1];
+		
+		insertElement(arr , 5 , 3);
+		
+		print(40);
+		
 		findTheElement(arr,6);
 		String print1  = Arrays.toString(arr);
 		System.out.println(print1);
@@ -94,6 +140,8 @@ public class ArrayOperation {
 		binarySearch(array, 5);
 		System.out.println(binarySearch(array, 5));
 		System.out.println(arr.hashCode());
+		
+		insertElement(arr, 10, 3);
 
 	}
 
