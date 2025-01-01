c8756476a1d9562fea16a8df8b00d79c6e6141c7
bhaveshkhose <khosebhavesh@gmail.com>
2025-01-01 22:25:08 +0530

    Reading data form file class

---
+++ File_Handling/ReadData.java
@@ -0,0 +1,32 @@
+package File_Handling;
+import java.io.*;
+
+public class ReadData {
+
+    public static void main(String [] args){
+        FileReader rd = null;
+    try{
+        rd = new FileReader("in.txt");
+        int character ;
+        while((character = rd.read()) != -1){
+            System.out.print((char)character);
+        }
+        
+
+    }
+    catch(IOException e){
+        e.printStackTrace();
+
+    }
+    finally{
+        try{
+            rd.close();
+
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    }
+    
+}
