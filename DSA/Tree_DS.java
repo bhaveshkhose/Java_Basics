d868c0730eefe45b498efbf2723e0386412068af
bhaveshkhose <khosebhavesh@gmail.com>
2025-01-10 11:22:55 +0530

    tree is added

--- DSA/Tree_DS.java
+++ DSA/Tree_DS.java
@@ -40,20 +40,23 @@ public class Tree_DS {
 	 //Method for print the tree in post-order
 	 public static void printTreePost(Node root) {
 		 if(root == null ) return ;
-		 
 		 size++;
 		 printTreePost(root.left);
 		 printTreePost(root.right);
-		 System.out.print(root.data +" ");
-		 
+		 System.out.print(root.data +" "); 
 	 }
-	 
 
-	
-	public static void main(String [] args) {
+	 
+	 public static void makeTree(int[] nums){
+		for(int i : nums){
+			if(i == -1){
+				return;
+			}
+		}
+	 }
+	public static void main(String [] bhavesh) {
 		
 		Node root;
-	
 		Node a = new Node(1);
 		Node b = new Node(2);
 		Node c = new Node(3);
@@ -61,14 +64,18 @@ public class Tree_DS {
 		Node e = new Node(5);
 		Node f = new Node(6);
 		Node g = new Node(7);
+		Node h = new Node(8);
+		Node i = new Node(9);
 		
 		root  =a  ;
 		a.left = b;
 		a.right = c;
 		b.left = d;
 		b.right = e;
-		c.left = f;
-		c.right = g;
+		e.left = f;
+		e.right = g;
+		f.right = i;
+		f.left = h;
 		
 		System.out.print("Tree traversing pre-order :- ");
 		printTreePre(root);
